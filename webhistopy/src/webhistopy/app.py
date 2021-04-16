e0db9718c4d0c5192b052839416e0709e3b78534
Felix Victor Münch <flxvctr@protonmail.com>
2021-04-16 17:50:17 +0200

    get Firefox in dev mode

--- webhistopy/src/webhistopy/app.py
+++ webhistopy/src/webhistopy/app.py
@@ -4,7 +4,7 @@ Experimental reconceptualisation of Webhistorian in Python
 from pathlib import Path
 from urllib.parse import urlparse
 
-from browser_history.browsers import Brave, Chrome
+from browser_history.browsers import Brave, Chrome, Firefox, Safari
 import pandas as pd
 import toga
 from toga.style import Pack
@@ -17,6 +17,7 @@ def bar_plot_domains(topdomains, path=None):
     fig = plot.get_figure()
     fig.savefig(path / 'figure.png', bbox_inches='tight')
 '''
+
 
 def get_domain(url):
     t = urlparse(url).netloc
@@ -37,7 +38,7 @@ class WebhistoPy(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        b = Chrome()
+        b = Firefox()
         output = b.fetch_history()
         history = output.histories
 
